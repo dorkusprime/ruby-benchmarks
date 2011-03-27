42a92a97550ca606f50d246ea39d184b7ff66ad3
Jevon Wild <jwild@ngmoco.com>
2011-03-26 18:03:22 -0700

    adding concat test file

---
+++ string_concatenation.rb
@@ -0,0 +1,33 @@
+require 'benchmark'
+
+n = 5000000
+Benchmark.bm do |x|
+  a = 'string1 '
+  b = 'string2 '
+  c = 'string3 '
+  d = 'string4 '
+  e = 'string5 '
+  f = nil
+  x.report('  +') { 
+   n.times do
+     f = nil
+     f = a + b + c + d + e
+   end
+  }
+  x.report(' +=') { 
+   n.times do
+     f = nil
+     f = a
+     f += b
+     f += c
+     f += d
+     f += e
+   end
+  }
+  x.report('#{}') { 
+   n.times do
+     f = nil
+     f = "#{a}#{b}#{c}#{d}#{e}"
+   end
+  }
+end
