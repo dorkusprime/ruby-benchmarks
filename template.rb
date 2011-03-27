181f4850996d0535cf872b3613df145615289737
Jevon Wild <jwild@ngmoco.com>
2011-03-26 17:54:25 -0700

    adding string_concatenation test

--- template.rb
+++ template.rb
@@ -2,9 +2,9 @@ require 'benchmark'
 
 n = 10000000
 Benchmark.bm do |x|
-   x.report('NAME HERE') { 
-     n.times do
-       # CODE HERE
-     end
-   }
+  x.report('NAME HERE') { 
+   n.times do
+     # CODE HERE
+   end
+  }
 end
